4fc420e1d49779eea6b3468649406ffaf57ce364
rishit-singh <rsrishitsingh@gmail.com>
2025-01-25 12:39:18 -0800

    Added ollama embeddings

--- src/tinyrag/embeddings.py
+++ src/tinyrag/embeddings.py
@@ -1,5 +1,39 @@
 from sentence_transformers import SentenceTransformer
+from tinytune.llmcontext import LLMContext
+
 import numpy
+
+from openai import OpenAI
+
+
+class EmbeddingContext(LLMContext):
+    def OnRun(self, *args, **kwargs):
+        tokens: list | None = kwargs.get("tokens")
+
+        if not (tokens):
+            raise Exception("Input tokens are missing")
+
+        pass
+
+    def __init__(self):
+        return
+
+    def __call__(self, *args, **kwargs):
+        return
+
+
+class OllamaEmbeddings:
+    def __init__(self, url: str, model: str):
+        self.Model: str = model
+        self.Client = OpenAI(base_url=url, api_key="ollama")
+
+    def Run(self, tokens: list) -> numpy.ndarray | None:
+        return numpy.array(
+            self.Client.embeddings.create(input=tokens, model=self.Model)
+        )
+
+    def __call__(self, tokens: list) -> numpy.ndarray | None:
+        return self.Run(tokens)
 
 
 class EmbeddingManager:
--- src/tinyrag/main.py
+++ src/tinyrag/main.py
@@ -1,10 +1,67 @@
-from embeddings import EmbeddingManager
+from embeddings import EmbeddingManager, OllamaEmbeddings
 from pprint import pprint
 
-pprint(
-    type(
-        EmbeddingManager("sentence-transformers/all-MiniLM-L6-v2")(
-            ["Hello mother fucker, is this shit fast enough"]
-        )
-    )
-)
+# from tinytune.contexts.gptcontext import GPTContext, openai
+
+# pprint(
+#     EmbeddingManager("CompendiumLabs/bge-base-en-v1.5-gguf").Generate(["Hello World"])
+
+# )
+
+embed = OllamaEmbeddings(
+    "http://localhost:11434/v1/",
+    "hf.co/CompendiumLabs/bge-base-en-v1.5-gguf",
+).Run(tokens=["hello", "world"])
+
+pprint(embed)
+
+# openai.base_url = "http://localhost:11434/v1/"
+
+# model = GPTContext("", "ollama")
+
+# model.Model.Name = "hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF"
+
+# model.OnGenerate = lambda content: print(content, end="") if content else None
+
+# model.Prompt(
+#     {
+#         "role": "system",
+#         "content": """
+#         You are a system that exclusively returns the function calls necessary to fulfill the user's request.
+
+#         Behavior:
+
+#         Parse the user's input and identify the most relevant function(s).
+#         Respond by outputting only the function call(s) with appropriate parameters.
+#         Do not include explanations, formatting, or additional text—only the function call(s).
+#         Refuse to generate any other text. Every response must be a function call. No extra content.
+#         If the users asks something beyond the scope, just ignore and print null.
+#         Example Input/Output:
+
+#         Input: "Calculate the area of a circle with radius 5."
+#         Output: calculate_circle_area(radius=5)
+
+#         Input: "Sort the numbers [3, 1, 4, 1, 5]."
+#         Output: sort_list(numbers=[3, 1, 4, 1, 5])
+
+#         Input: "Find the square root of 9."
+#         Output: calculate_sqrt(value=9)
+#     """,
+#     }
+# ).Run(stream=True)
+
+
+# i = input("\n>")
+
+# while i != "exit":
+#     model.Prompt({"role": "user", "content": i}).Run(stream=True)
+
+#     i = input("\n> ")
+
+# pprint(
+#     type(
+#         EmbeddingManager("sentence-transformers/all-MiniLM-L6-v2")(
+#             ["Hello mother fucker, is this shit fast enough"]
+#         )
+#     )
+# )
